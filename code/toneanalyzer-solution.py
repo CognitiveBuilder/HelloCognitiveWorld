a71eb2bb6b7f3b6c6efb73fec3d6532dd44926af
J.F. Omhover <jf.omhover@galvanize.com>
2017-05-13 04:06:46 -0700

    new cleaner code

--- code/toneanalyzer-solution.py
+++ code/toneanalyzer-solution.py
@@ -2,6 +2,7 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
 
+
 # BEGIN of python-dotenv section
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -18,22 +19,41 @@ tone_analyzer = ToneAnalyzerV3(
    version='2016-05-19')
 
 
-while 1:
-    input_content = input('ToneAnalyzer> ')
+def parse_toneanalyzer_response(json_data):
+    """Parses the JSON response from ToneAnalyzer to return
+    a dictionary of emotions and their corresponding score.
 
-    if (input_content.lower() in {'exit', 'quit', 'q', 'n'}):
-        break
+    Parameters
+    ----------
+    json_data: {dict} a json response from ToneAnalyzer (see Notes)
 
-    json_response = tone_analyzer.tone(text=input_content)
-    #print(json.dumps(json_response, indent=2))
-
+    Returns
+    -------
+    dict : a {dict} whose keys are emotion ids and values are their corresponding score.
+    """
     emotions = {}
-    for entry in json_response['document_tone']['tone_categories']:
+    for entry in json_data['document_tone']['tone_categories']:
         if entry['category_id'] == 'emotion_tone':
             for emotion in entry['tones']:
                 emotion_key = emotion['tone_name']
                 emotion_value = emotion['score']
                 emotions[emotion_key] = emotion_value
+    return(emotions)
 
-    for key in ['Anger','Disgust','Fear','Joy','Sadness']:
-        print("{0}, {1:0.2f}".format(key, emotions[key]))
+
+if __name__ == '__main__':
+    while 1:
+        # use line below if you're in python 2
+        # input_content = raw_input('ToneAnalyzer>').strip()
+
+        # get some sentence from user input
+        input_content = input('ToneAnalyzer> ')
+
+        # if you type one of these, you exit the script
+        if (input_content.lower() in {'exit', 'quit', 'q', 'n'}):
+            break
+
+
+        json_response = tone_analyzer.tone(text=input_content)
+        #print(json.dumps(json_response, indent=2))
+        print(parse_toneanalyzer_response(json_response))
--- code/toneanalyzer.py
+++ code/toneanalyzer.py
@@ -2,6 +2,7 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
 
+
 # BEGIN of python-dotenv section
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -18,11 +19,39 @@ tone_analyzer = ToneAnalyzerV3(
    version='2016-05-19')
 
 
-while 1:
-    input_content = input('ToneAnalyzer> ')
+def parse_toneanalyzer_response(json_data):
+    """Parses the JSON response from ToneAnalyzer to return
+    a dictionary of emotions and their corresponding score.
 
-    if (input_content.lower() in {'exit', 'quit', 'q', 'n'}):
-        break
+    Parameters
+    ----------
+    json_data: {dict} a json response from ToneAnalyzer (see Notes)
 
-    json_response = tone_analyzer.tone(text=input_content)
-    print(json.dumps(json_response, indent=2))
+    Returns
+    -------
+    dict : a {dict} whose keys are emotion ids and values are their corresponding score.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    while 1:
+        # get some sentence from user input
+        input_content = input('ToneAnalyzer> ')
+
+        # use line below instead if you're in python 2
+        # input_content = raw_input('ToneAnalyzer>').strip()
+
+        # if you type one of these, you exit the script
+        if (input_content.lower() in {'exit', 'quit', 'q', 'n'}):
+            break
+
+
+        # ask ToneAnalyzer to analyse the input
+        json_response = tone_analyzer.tone(text=input_content)
+
+        # dumping the raw JSON response
+        print(json.dumps(json_response, indent=2))
+
+        # printing the result from the parsing function
+        print(parse_toneanalyzer_response(json_response))
